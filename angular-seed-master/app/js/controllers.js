73b60c319de7b46db00d423d2590ce9d3a7dda38
Stormcoder <teryaevs@yandex.ru>
2020-02-25 21:52:18 +0300

    добавлен FinanceController

--- angular-seed-master/app/js/controllers.js
+++ angular-seed-master/app/js/controllers.js
@@ -2,10 +2,11 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+angular.module('myApp.controllers', [])
+    .controller('FinanceController', function ($scope) {
+        $scope.salary = 0;
+        $scope.percentage = 0;
+        $scope.result = function () {
+            return $scope.salary * $scope.percentage * 0.01;
+        };
+    });
