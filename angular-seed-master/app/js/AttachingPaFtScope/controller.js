799774430ef2abbcc71af43f6791f94c9bf13c38
Teryaev Sergey <teryaevs@yandex.ru>
2020-02-27 10:21:18 +0300

    Добавлен GreetingController

---
+++ angular-seed-master/app/js/AttachingPaFtScope/controller.js
@@ -0,0 +1,8 @@
+//
+
+angular.module('myApp.greeting', [])
+    .controller('GreetingController', function ($scope) {
+        $scope.now = new Date(); //set the model 'now' on scope
+        $scope.greeting = 'Hello'; //set the name model on scope
+    })
+;
