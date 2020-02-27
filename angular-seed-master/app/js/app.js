f3a616da79c52e55b97c8296a3ec24141a44ec1c
Teryaev Sergey <teryaevs@yandex.ru>
2020-02-27 13:10:58 +0300

    исправлен косяк с зависимостями

--- angular-seed-master/app/js/app.js
+++ angular-seed-master/app/js/app.js
@@ -7,8 +7,7 @@ angular.module('myApp', [
     'myApp.filters',
     'myApp.services',
     'myApp.directives',
-    'myApp.controllers',
-    'myApp.greeting'
+    'myApp.controllers'
 ]).config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
