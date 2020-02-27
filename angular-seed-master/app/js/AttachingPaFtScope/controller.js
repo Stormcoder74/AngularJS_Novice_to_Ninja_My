0a84a9e357f6b93847b29022905a817237cdbe61
Teryaev Sergey <teryaevs@yandex.ru>
2020-02-27 11:34:42 +0300

    добавлена функция выбора случайного языка в GreetingController

--- angular-seed-master/app/js/AttachingPaFtScope/controller.js
+++ angular-seed-master/app/js/AttachingPaFtScope/controller.js
@@ -3,6 +3,11 @@
 angular.module('myApp.greeting', [])
     .controller('GreetingController', function ($scope) {
         $scope.now = new Date(); //set the model 'now' on scope
-        $scope.greeting = 'Hello'; //set the name model on scope
+        $scope.helloMessages = ['Hello', 'Bonjour', 'Hola', 'Ciao', 'Hallo'];
+        $scope.greeting = $scope.helloMessages[0];
+
+        $scope.getRandomHelloMessage = function() {
+            $scope.greeting = $scope.helloMessages[parseInt((Math.random() * $scope.helloMessages.length))];
+        }
     })
 ;
