d83ac82d40a81502dd2d52cce707329d071c7cde
Teryaev Sergey <teryaevs@yandex.ru>
2020-02-28 13:21:13 +0300

    The Watchers in AngularJS

--- angular-seed_scopes/app/js/controllers.js
+++ angular-seed_scopes/app/js/controllers.js
@@ -13,4 +13,18 @@ angular.module('myApp.controllers')
     .controller('BookController', function ($scope) {
         $scope.books = ['Jump Start HTML5', 'Jump Start CSS', 'Jump Start Responsive Web Design'];
         $scope.name = "Scope for BookController";
+
+        $scope.wishListCount = 0;
+        $scope.addToWishList = function (book) {
+            $scope.wishListCount++;
+        };
+        var unbindWatcher = $scope.$watch('wishListCount', function (newValue, oldValue) {
+            if (newValue !== oldValue) {
+                console.log("wish list increased from " + oldValue + " to " + newValue);
+                if (newValue === 2) {
+                    alert('Great! You have 2 items in your wish list. Time to buy what you love. ');
+                    unbindWatcher();
+                }
+            }
+        });
     });
