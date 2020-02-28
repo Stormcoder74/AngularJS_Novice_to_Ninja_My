66f6fe42e674efb4ae35a16a735202b1c5548bc1
Teryaev Sergey <teryaevs@yandex.ru>
2020-02-28 17:22:25 +0300

    $apply and $digest in Action

--- angular-seed_scopes/app/js/controllers.js
+++ angular-seed_scopes/app/js/controllers.js
@@ -28,3 +28,15 @@ angular.module('myApp.controllers')
             }
         });
     });
+
+angular.module('myApp')
+    .controller('TimeoutController', function ($scope) {
+        $scope.scheduleTask = function () {
+            setTimeout(function () {
+                $scope.$apply(function () { // wrapped the code in $apply()
+                    $scope.message = 'Fetched after 3 seconds';
+                    console.log('message = ' + $scope.message); //log this to console
+                });
+            }, 3000);
+        };
+    });
