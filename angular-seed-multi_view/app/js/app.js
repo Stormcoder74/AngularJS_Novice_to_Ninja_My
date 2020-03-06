95a8913682bbde2ea50d33dbdd12c04320f753f3
Teryaev Sergey <teryaevs@yandex.ru>
2020-03-06 11:39:30 +0300

    Using ng-template

--- angular-seed-multi_view/app/js/app.js
+++ angular-seed-multi_view/app/js/app.js
@@ -10,14 +10,18 @@ angular.module('myApp', [
 angular.module('myApp')
     .config(function ($routeProvider, $locationProvider) {
         $routeProvider
+            .when('/start', {
+                controller: 'Controller',
+                templateUrl: '/index.html'
+            })
             .when('/view1', {
                 controller: 'Controller1',
-                templateUrl: '/partials/view1.html'
+                templateUrl: '/view1.tpl' //The ng-template id
             })
             .when('/view2/:firstname/:lastname', {
                 controller: 'Controller2',
-                templateUrl: '/partials/view2.html'
+                templateUrl: '/view2.tpl' //The ng-template id
             })
-            .otherwise({redirectTo: '/view1'});
+            .otherwise({redirectTo: '/'});
         $locationProvider.html5Mode(true); //activate HTML5 Mode
     });
